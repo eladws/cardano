03457664af225fffaef63a2bb9ebfa6a810cb769
eladws <elad@salt.security>
2022-01-13 23:28:00 +0200

    fixing core node docker compose file

--- topology_pull.sh
+++ topology_pull.sh
@@ -2,7 +2,7 @@
 
 # this script downloads the latest list of validated peers, and restarts the node if neccesary
 
-my_block_producing_ip="127.0.0.1" # TODO: CHANGE ME IF CONNECTED TO A CORE NODE !
+my_block_producing_ip="127.0.0.1" # TODO: CHANGE ME IF CONNECTED TO A CORE NODE
 my_block_producing_port="5555"
 topology_file="cardano/topology-updates/mainnet-topology.json"
 curl -s -o ${topology_file}.new "https://api.clio.one/htopology/v1/fetch/?max=20&customPeers=${my_block_producing_ip}:${my_block_producing_port}:1|relays-new.cardano-mainnet.iohk.io:3001:2"
@@ -15,6 +15,5 @@ else
   echo "topology got updated"
   echo $(date) > cardano/topology-updates/TOPOLOGY_UPDATED
   cp ${topology_file}.new ${topology_file}
-  # TODO: restart node using the updated topology
 fi
 rm ${topology_file}.new
