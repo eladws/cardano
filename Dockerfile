45054dc884915f261fe7ffb96bc9afe9055385e2
eladws <elad@salt.security>
2022-01-18 22:08:54 +0200

    single setup but different docker compose files

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM ubuntu
 
 ENV CARDANO_NODE_HOME=/cardano
-ENV CARDANO_NODE_CONFIG_FILE=${CARDANO_NODE_HOME}/configuration/cardano/mainnet-config.json
 ENV PATH="${CARDANO_NODE_HOME}:${PATH}"
 
 WORKDIR ${CARDANO_NODE_HOME}
@@ -13,15 +12,12 @@ RUN wget https://hydra.iohk.io/job/Cardano/cardano-node/cardano-node-linux/lates
 
 RUN tar -xf binary-dist
 
-# Customize configuration
-RUN sed -i 's/127.0.0.1/0.0.0.0/g' ${CARDANO_NODE_CONFIG_FILE}
-RUN sed -i 's/TraceBlockFetchDecisions\": false/TraceBlockFetchDecisions\": true/g' ${CARDANO_NODE_CONFIG_FILE}
-
 # Using GC parameters from https://forum.cardano.org/t/solving-the-cardano-node-huge-memory-usage-done/67032
 CMD ["/cardano/cardano-node", "run", \
     "+RTS", "-N2", "--disable-delayed-os-memory-return", "-I0.3", \
     "-Iw600", "-A16m", "-F1.5", "-H2500M", "-T", "-S", "-RTS", \
     "--topology", "/node-topology/mainnet-topology.json", \
+    "--config", "/node-config/mainnet-config.json", \
     "--database-path", "/node-db", \
     "--socket-path", "/node-ipc/node.socket", \
     "--port", "6666"]
---
+++ setup.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+node_image="custom"
+node_mode="relay"
+while getopts i:m: flag
+do
+    case "${flag}" in
+        i) node_image=${OPTARG};;
+        m) node_mode=${OPTARG};;
+    esac
+done
+
+# install useful packages
+apt-get update
+apt-get install -y ca-certificates curl gnupg lsb-release wget git jq
+
+# install docker
+if ! command -v docker
+then
+    echo "docker does not exist. installing docker..."
+
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+    
+    apt-get update
+    apt-get install -y docker-ce docker-ce-cli containerd.io
+else
+    echo "docker is already installed"
+fi
+
+# install docker-compose
+if ! command -v docker-compose
+then
+    echo "docker-compose does not exist. installing docker-compose..."
+    curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+else
+    echo "docker is already installed"
+fi
+
+# clone git repo if it is not present
+if [ ! -d "cardano" ] 
+then
+    echo "Cloning repo..."
+    apt-get install -y git
+    git clone https://github.com/eladws/cardano.git cardano
+    chmod +x cardano/*.sh
+fi
+
+# downlaod default configuration
+mkdir cardano/node-config
+wget -O cardano/node-config/mainnet-config.json https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-config.json
+wget -O cardano/node-config/mainnet-byron-genesis.json https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-byron-genesis.json
+wget -O cardano/node-config/mainnet-shelley-genesis.json https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-shelley-genesis.json
+wget -O cardano/node-config/mainnet-alonzo-genesis.json https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-alonzo-genesis.json
+
+# make required changes to configuration file
+sed -i 's/127.0.0.1/0.0.0.0/g' cardano/node-config/mainnet-config.json
+sed -i 's/TraceBlockFetchDecisions\": false/TraceBlockFetchDecisions\": true/g' cardano/node-config/mainnet-config.json
+
+if [ node_mode = "relay" ];
+    # set topology updates and daily restarts as cron jobs
+    echo "55 * * * * ./cardano/topology_push.sh" > crontab.txt
+    echo "5 5 * * * ./cardano/topology_pull.sh" >> crontab.txt
+    echo "* 6 * * * sudo docker-compose -f cardano/docker-compose-custom-image.yaml restart relay-node" >> crontab.txt
+    crontab crontab.txt
+elif [ node_mode = "core" ];
+    # check required keys exist
+    # check files required to run a core-node
+    if [ ! -d "cardano/node-keys" ] 
+    then
+        echo "Cannot run without node-keys folder!"
+        exit 1
+    fi
+
+    if [ ! -f "cardano/node-keys/kes.skey" ]; then
+        echo "Missing kes.skey!"
+        exit 1
+    fi
+
+    if [ ! -f "cardano/node-keys/vrf.skey" ]; then
+        echo "Missing vrf.skey!"
+        exit 1
+    fi
+
+    if [ ! -f "cardano/node-keys/node.cert" ]; then
+        echo "Missing node.cert!"
+        exit 1
+    fi
+else
+    echo "Unknown node mode $node_mode. Please specify relay (default) or core."
+    exit 1
+fi
+
+# TODO: make a single docker compose with all components, with configurable image, and optional core-node !
+echo "Starting docker $node_mode node using node-image $node_image ..."
+if [ node_mode = "relay" ];
+    sudo docker-compose -f cardano/docker-compose-$node_image-image.yaml up -d
+elif [ node_mode = "core" ];
+    sudo docker-compose -f cardano/docker-compose-core-node.yaml up -d
+fi
